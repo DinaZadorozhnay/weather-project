d86b97839ce8c4ff83cbae5440ad360b7904fc94
DinaZadorozhnay <dizadorozhnay@gmail.com>
2023-10-18 21:35:16 +0200

    added APIforecast, moved variables inside functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 let weatherApiKey = "7746bdeabca928cfedcad71e52fd9d66";
 let timeApiKey = "6XCJExCg6xl9Msle0jbAgg==RLPL7Ko2QBEMoXTz";
 
-let currentMeasureElement = document.querySelector("#currentMeasure");
 let changeMeasureButton = document.querySelector("#changeMeasure");
-let currentTempElement = document.querySelector("#currentTemp");
-let todayTempMaxElement = document.querySelector("#today-max");
-let todayTempMinElement = document.querySelector("#today-min");
-let todayDescriptionElement = document.querySelector("#today-description");
-let todayWindElement = document.querySelector("#today-wind");
-let todayHumidityElement = document.querySelector("#today-humidity");
-let currentDateElement = document.querySelector("#date");
 let currentLocationButton = document.querySelector("#currentLocationButton");
-let currentCityElement = document.querySelector("#currentCity");
-let currentIconElement = document.querySelector("#icon");
 let searchFormElement = document.querySelector("#searchForm");
-let searchInputElement = document.querySelector("#searchInput");
 
 let celsTemp = null;
 let todayTempMax = null;
@@ -25,6 +14,14 @@ ShowWeather and date, time functions
  */
 
 function showWeather(response) {
+  let currentTempElement = document.querySelector("#currentTemp");
+  let todayTempMaxElement = document.querySelector("#today-max");
+  let todayTempMinElement = document.querySelector("#today-min");
+  let todayDescriptionElement = document.querySelector("#today-description");
+  let todayWindElement = document.querySelector("#today-wind");
+  let todayHumidityElement = document.querySelector("#today-humidity");
+  let currentCityElement = document.querySelector("#currentCity");
+  let currentIconElement = document.querySelector("#icon");
   celsTemp = response.data.main.temp;
   todayTempMax = response.data.main.temp_max;
   todayTempMin = response.data.main.temp_min;
@@ -43,6 +40,11 @@ function showWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${icon}@2x.png`
   );
+  getForecast(response.data.coord);
+}
+function getForecast(coordinates) {
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={current,minutely,hourly,alerts}&appid=${weatherApiKey}&&units=metric`;
+  axios.get(weatherUrl).then(showForecast);
 }
 function roundTemp(temp) {
   temp = Math.round(temp);
@@ -52,6 +54,7 @@ function roundTemp(temp) {
   return temp;
 }
 function showDate(response) {
+  let currentDateElement = document.querySelector("#date");
   let day = response.data.day_of_week;
   let hour = response.data.hour;
   let minute = response.data.minute;
@@ -64,6 +67,7 @@ function showDate(response) {
  * changing measures
  */
 function changeMeasures() {
+  let currentMeasureElement = document.querySelector("#currentMeasure");
   let measureForChange = currentMeasureElement.innerHTML;
   currentMeasureElement.innerHTML = changeMeasureButton.innerHTML;
   changeMeasureButton.innerHTML = measureForChange;
@@ -73,6 +77,10 @@ function convertToFar(temp) {
   return roundTemp(temp);
 }
 function changeTemp() {
+  let currentMeasureElement = document.querySelector("#currentMeasure");
+  let currentTempElement = document.querySelector("#currentTemp");
+  let todayTempMaxElement = document.querySelector("#today-max");
+  let todayTempMinElement = document.querySelector("#today-min");
   if (currentMeasureElement.innerHTML.toString().trim() === "°C") {
     currentTempElement.innerHTML = roundTemp(celsTemp);
     todayTempMaxElement.innerHTML = roundTemp(todayTempMax);
@@ -101,17 +109,20 @@ function showCity(city) {
     .then(showDate);
 }
 function changeCity(event) {
+  let searchInputElement = document.querySelector("#searchInput");
   event.preventDefault();
   showCity(searchInputElement.value);
 }
 
 /**current location weather */
 function showCurrentLocationWeather(event) {
+  let searchInputElement = document.querySelector("#searchInput");
   navigator.geolocation.getCurrentPosition(function (position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&&units=metric`;
     let timeUrl = `https://api.api-ninjas.com/v1/worldtime?`;
+
     axios.get(weatherUrl).then(showWeather);
     axios
       .get(timeUrl, {
@@ -122,30 +133,50 @@ function showCurrentLocationWeather(event) {
   });
   searchInputElement.value = null;
 }
-
-function showForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function showForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastTableElement = document.querySelector("#forecastTable");
   let forecastHTML = `<div class="row days">`;
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col weatherTable">
-                <div class="day">${day}</div>
-                <img src="img/cloud.svg" class="tableIMG" alt="" width="50px" />
+                <div class="day">${formatDay(forecastDay.dt)}</div>
+                <img src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" class="tableIMG" alt="" width="50px" />
 
                 <div class="labels">
-                  <span class="label">MIN +26</span>
-                  <span class="label">MAX +26</span>
+                  <span class="label">MIN <span>${roundTemp(
+                    forecastDay.temp.min
+                  )}</span></span>
+              <span class="label">MAX <span>${roundTemp(
+                forecastDay.temp.max
+              )}</span></span>
                 </div>
                 <ul class="weatherDetails">
-                  <li class="description">Partly cloudy</li>
-                  <li>Wind <span class="wind">16</span> m/s</li>
-                  <li>Humidity <span class="humidity">75</span>%</li>
+                  <li class="description">${
+                    forecastDay.weather[0].description
+                  }</li>
+                  <li>Wind <span class="wind">${Math.round(
+                    forecastDay.wind_speed
+                  )}</span> m/s</li>
+                  <li>Humidity <span class="humidity">${
+                    forecastDay.humidity
+                  }</span>%</li>
                 </ul>
               </div>
             `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastTableElement.innerHTML = forecastHTML;
@@ -161,4 +192,3 @@ changeMeasureButton.addEventListener("click", function () {
 searchFormElement.addEventListener("submit", changeCity);
 
 showCity("New York");
-showForecast();
